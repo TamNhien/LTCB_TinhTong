c0036513ea2a510b45308dcae2f74dd1f8a44907
ADMIN <ADMIN@LAB1-MAY15>
2024-02-26 08:27:10 +0700

    Add project files.

---
+++ LTCB_TinhTong.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int so1, so2, tong;
+	cout << "Chuong trinh tinh tong\n";
+	cout << " Nhap so thu nhat : ";
+	cin >> so1;
+	cout << "Nhap so thu hai : ";
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << " + " << so2 << " = " << tong << endl;
+	return 0;
+}
